79a63ce5a82ab9e5b073a0d01d927cec93b12174
Kevin Hui <kevinhuitaylors@gmail.com>
2019-04-10 12:10:56 +0800

    installed faker gem and created seeds files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do |blog|
+  Blog.create!(
+    title: "This is a post about #{Faker::Games::Fallout.unique.character}",
+    body: Faker::Games::Fallout.quote
+  )
+end
+puts "=== 10 Blog posts created ==="
+
+9.times do |portfolio_items|
+  Portfolio.create!(
+    title: Faker::Games::Witcher.unique.character,
+    subtitle: Faker::Games::Witcher.location,
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,   sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    main_image: "https://via.placeholder.com/600x400",
+    thumb_image: "https://via.placeholder.com/300x250"
+  )
+end
+puts "=== 9 Portfolio items created ==="
+
+5.times do |skills|
+  Skill.create!(
+    title: Faker::Games::Fallout.faction,
+    percent_utilized: rand(1..100)
+  )
+end
+puts "=== 5 skills created ==="

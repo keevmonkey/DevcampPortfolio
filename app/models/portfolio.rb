c92fbca1c67cc2e8c50134dc73cb4f81902e5141
Kevin Hui <kevinhuitaylors@gmail.com>
2019-05-04 21:33:55 +0800

    coded after_initialize :set_defaults for images on portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,12 +1,18 @@
 class Portfolio < ApplicationRecord
-
   validates :title, :body, presence: true
   validates_presence_of :main_image, :thumb_image
+
+  after_initialize :set_defaults
 
   def self.angular
     where(subtitle: 'Angular')
   end
 
   scope :non_angular, -> { where.not(subtitle: 'Angular') }
-  
+
+  def set_defaults
+    self.main_image   ||= "https://via.placeholder.com/600x400"
+    self.thumb_image  ||= "https://via.placeholder.com/300x250"  
+  end
+
 end

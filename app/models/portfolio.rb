9c2d46474222a13fa1ff6de3a30a888d613cbf3d
Kevin Hui <kevinhuitaylors@gmail.com>
2019-05-04 21:41:56 +0800

    added badge to skills and implemented concern for managing image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates :title, :body, presence: true
   validates_presence_of :main_image, :thumb_image
 
@@ -11,8 +12,8 @@ class Portfolio < ApplicationRecord
   scope :non_angular, -> { where.not(subtitle: 'Angular') }
 
   def set_defaults
-    self.main_image   ||= "https://via.placeholder.com/600x400"
-    self.thumb_image  ||= "https://via.placeholder.com/300x250"  
+    self.main_image   ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image  ||= Placeholder.image_generator(height: '300', width: '250')
   end
 
 end

ef3c40cf46449a4b930996779a8a7d37f8c585e4
Kevin Hui <kevinhuitaylors@gmail.com>
2019-05-12 14:17:03 +0800

    integrated actionview helper method for views

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,4 +5,9 @@ class Blog < ApplicationRecord
   enum status: { draft: 0, published: 1 }
   validates_presence_of :title, :body
   belongs_to :topic
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.topic ||= Topic.first 
+  end
 end

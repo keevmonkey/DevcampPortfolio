393d3678a0e992c1bd1a6bb741e8cb2e0b7f69fc
Kevin Hui <kevinhuitaylors@gmail.com>
2019-05-20 18:30:22 +0800

    updated image defaults for portfolio to work with carrierwave

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,2 +1,17 @@
 module PortfoliosHelper
+  def image_generator(height:, width:)
+    "https://via.placeholder.com/#{height}x#{width}"
+  end
+
+  def portfolio_image image, type
+    if image 
+      image
+    elsif type == 'thumb'
+      image_generator(height: '300', width: '250')
+    else
+      image_generator(height: '600', width: '400')
+    end
+  end
 end
+
+
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,4 @@
 class Portfolio < ApplicationRecord
-  include Placeholder
   has_many :technologies, dependent: :destroy
   # configure model to accept nested data for technology model
   # afterwhich, sets basic first validation to reject if attars['name'] is blank
@@ -7,7 +6,6 @@ class Portfolio < ApplicationRecord
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
 
   validates :title, :body, presence: true
-  validates_presence_of :main_image, :thumb_image
 
   scope :non_angular, -> { where.not(subtitle: 'Angular') }
 
@@ -21,12 +19,4 @@ class Portfolio < ApplicationRecord
   def self.by_position
     order('position ASC')
   end
-
-  after_initialize :set_defaults
-
-  def set_defaults
-    self.main_image   ||= Placeholder.image_generator(height: '600', width: '400')
-    self.thumb_image  ||= Placeholder.image_generator(height: '300', width: '250')
-  end
-
 end
